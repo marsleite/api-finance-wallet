8b2069ef7f25795eb4c01ed9272706ef51a4616c
Marcelo <marcelo.leite@mercadolivre.com>
2023-01-21 23:43:37 -0300

    [feat] initial commit

---
+++ src/test/kotlin/com/finance/wallet/FinanceWalletApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.finance.wallet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceWalletApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
